89552ed3d0be109db3ff8837a6620b21933ee080
itsshashwatsingh <shashwat98k@gmail.com>
2025-07-23 14:06:08 +0530

    'etl'

---
+++ scripts/load_all_data.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:: Set PostgreSQL credentials
+set PGUSER=postgres
+set PGPASSWORD=password
+set PGDB=datawarehouse
+
+:: Set path to psql.exe
+cd /d "C:\Program Files\PostgreSQL\17\bin"
+
+:: Capture start time in seconds
+for /f %%a in ('powershell -command "[int](Get-Date).ToUniversalTime().Subtract([datetime]'1970-01-01').TotalSeconds"') do set START=%%a
+
+echo =====================================================
+echo  Starting Data Load into Bronze Layer...
+echo =====================================================
+
+:: Execute SQL script
+psql -U %PGUSER% -d %PGDB% -f "C:\Users\fusio\Desktop\Data_warehouse_project\SQL-Data-Warehouse-Project\scripts\load_all_data.sql"
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo  Error: Failed to load bronze layer data. Please check your .sql script or CSV paths.
+    pause
+    exit /b 1
+) ELSE (
+    :: Capture end time
+    for /f %%b in ('powershell -command "[int](Get-Date).ToUniversalTime().Subtract([datetime]'1970-01-01').TotalSeconds"') do set END=%%b
+
+    :: Calculate duration
+    set /a ELAPSED=!END! - !START!
+echo ======================================================
+echo  Load completed successfully.
+echo  Total time taken: !ELAPSED! seconds.
+echo ======================================================
+)
+
+pause
+endlocal
+
---
+++ scripts/load_all_data.sql
@@ -0,0 +1,18 @@
+TRUNCATE TABLE bronze.crm_cust_info;
+\COPY bronze.crm_cust_info FROM 'C:/Users/fusio/Desktop/Data_warehouse_project/SQL-Data-Warehouse-Project/datasets/source_crm/cust_info.csv' WITH (FORMAT csv, HEADER true);
+
+TRUNCATE TABLE bronze.crm_prd_info;
+\COPY bronze.crm_prd_info FROM 'C:/Users/fusio/Desktop/Data_warehouse_project/SQL-Data-Warehouse-Project/datasets/source_crm/prd_info.csv' WITH (FORMAT csv, HEADER true);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+\COPY bronze.crm_sales_details FROM 'C:/Users/fusio/Desktop/Data_warehouse_project/SQL-Data-Warehouse-Project/datasets/source_crm/sales_details.csv' WITH (FORMAT csv, HEADER true);
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+\COPY bronze.erp_cust_az12 FROM 'C:/Users/fusio/Desktop/Data_warehouse_project/SQL-Data-Warehouse-Project/datasets/source_erp/CUST_AZ12.csv' WITH (FORMAT csv, HEADER true);
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+\COPY bronze.erp_loc_a101 FROM 'C:/Users/fusio/Desktop/Data_warehouse_project/SQL-Data-Warehouse-Project/datasets/source_erp/LOC_A101.csv' WITH (FORMAT csv, HEADER true);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+\COPY bronze.erp_px_cat_g1v2 FROM 'C:/Users/fusio/Desktop/Data_warehouse_project/SQL-Data-Warehouse-Project/datasets/source_erp/PX_CAT_G1V2.csv' WITH (FORMAT csv, HEADER true);
+
