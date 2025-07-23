5d171ff1d4bc50d8e3326d302b8c4c4862dfef15
Shashwat Singh <shashwat98k@gmail.com>
2025-07-23 14:17:45 +0530

    Update 01_init_database.sql

--- scripts/01_init_database.sql
+++ scripts/01_init_database.sql
@@ -1,8 +1,6 @@
 /*
 Create Database and Schemas
-
-=========================================================================
-
+=================================================================================================================
 Script Purpose:
 	This script creates a new database named 'DataWarehouse' after checking if it already exists.
 	If the database exists, it will be dropped and recreated. Additionally, the script sets up three schemas
@@ -13,6 +11,7 @@ WARNING:
 	Running this script will drop the entire 'DataWarehouse' database if it exists.
 	All data in the database will be permenently deleted. Proceed with caution
 	and ensure you have proper backups before running these script.
+=================================================================================================================
 */
 
 
